30558226f94e180e671eabcf3f9e8881718d45eb
p3nguin224 <helloworld.22456@gmail.com>
2021-11-25 12:33:34 +0630

    login_complete

--- JobPortal/src/main/java/com/jobportal/user/controller/HomeController.java
+++ JobPortal/src/main/java/com/jobportal/user/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.jobportal.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,20 @@ public class HomeController {
 	@RequestMapping("/")
 	private String homePage() {
 		return "index";
+	}
+	
+	@RequestMapping("/index")
+	private String index() {
+		return "redirect:/";  // redirect to home page if it comes with index
+	}
+	
+	// method name is not important as Controller works with mapping
+	// RequestMapping will be PostMapping if client HTTP Request is differrnt
+	// Model object is used to pass data to UI... Same as request.setParameter from javaEE
+	@RequestMapping("/login")
+	private String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
+		return "myAccount";	
 	}
 
 }
