5dc4f2ee784c7c79836e6ae65d5747fa40a747fd
p3nguin224 <helloworld.22456@gmail.com>
2021-11-25 12:02:37 +0630

    login_finish_with_temp_template

--- JobPortal/src/main/java/com/jobportal/user/config/SecurityConfig.java
+++ JobPortal/src/main/java/com/jobportal/user/config/SecurityConfig.java
@@ -1,5 +1,69 @@
 package com.jobportal.user.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.jobportal.user.service.impl.UserSecurityService;
+import com.jobportal.user.utility.SecurityUtility;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserSecurityService userSecurityService;
+	
+	// AuthenticationManagerBuiler will need encoder to check
+	private BCryptPasswordEncoder passwordEncoder() {
+		return SecurityUtility.passwordEncoder();
+	}
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/css/**",
+			"/js/**",
+			"/image/**",
+			"/", 
+			"/index",
+			"/newUser",
+			"/forgetPassword",
+			"/login",
+			"/fonts/**",
+			"/bookshelf",
+			"/bookDetail",
+			"/hours"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		
+		// Security auto block server-side attack which pulls json objects
+		// this security need to be block while developing state;
+		http.csrf().disable().cors().disable()
+		.formLogin()                  // Log in Form configuration start
+		.failureUrl("/login?error")   // landing page after unsuccessful login
+		.loginPage("/login").permitAll()  // Custom login page and permit it to all users
+		.and()     // login config end
+		.logout()                     // Log out Form configuration start
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))   // mapping that triggers "log out" to occur.
+		.logoutSuccessUrl("/?logout")  // page that will show when logout success
+		.deleteCookies("remember-me").permitAll()  // delete cookies after logout
+		.and()    // logout config end
+		.rememberMe();	 // Browser will ask user for RememberMe option
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
 
 }
--- JobPortal/src/main/java/com/jobportal/user/domain/User.java
+++ JobPortal/src/main/java/com/jobportal/user/domain/User.java
@@ -1,6 +1,10 @@
 package com.jobportal.user.domain;
 
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,11 +15,20 @@ import javax.persistence.Id;
 
 import javax.persistence.OneToOne;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.jobportal.user.domain.security.Authority;
 import com.jobportal.user.domain.security.UserRole;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(nullable = false, updatable = false)
@@ -28,13 +41,59 @@ public class User {
 	private String email;
 	private String registeredDate;
 	private String status;
+	private Boolean isEnable = true;
 	
 	@OneToOne(mappedBy = "user",fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	private UserRole userRole;
 	
 	public User() {
 		
-	}	
+	}
+
+	// Need to change here if role doesn't work
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new Authority(userRole.getRole().getName()));
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return isEnable;
+	}
 
 	public Long getUserId() {
 		return userId;
@@ -42,22 +101,6 @@ public class User {
 
 	public void setUserId(Long userId) {
 		this.userId = userId;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getDob() {
@@ -100,6 +143,14 @@ public class User {
 		this.status = status;
 	}
 
+	public Boolean getIsEnable() {
+		return isEnable;
+	}
+
+	public void setIsEnable(Boolean isEnable) {
+		this.isEnable = isEnable;
+	}
+
 	public UserRole getUserRole() {
 		return userRole;
 	}
@@ -107,6 +158,18 @@ public class User {
 	public void setUserRole(UserRole userRole) {
 		this.userRole = userRole;
 	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	
+	
+
+	
 
 	
 	
